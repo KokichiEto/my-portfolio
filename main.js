89c242f7950d835ba5c6c0eb5285138a6926faeb
KokichiEto <e.kokichi0428@gmail.com>
2023-05-02 10:00:20 +0900

    ポートフォリオサイトの開発を進めました

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const Home = document.getElementById("Home");
 const Game = document.getElementById("Game");
 const Carp = document.getElementById("Carp");
-const Instagram = document.getElementById("Instagram");
+const UIUXStady = document.getElementById("UIUXStady");
 Home.onclick = function () {
   window.location.href = "Home.html";
 };
@@ -11,7 +11,6 @@ Game.onclick = function () {
 Carp.onclick = function () {
   window.location.href = "https://www.carp.co.jp/";
 };
-Instagram.onclick = function () {
-  window.location.href =
-    "https://instagram.com/ko_kichi2001?igshid=ZDdkNTZiNTM=";
+UIUXStady.onclick = function () {
+  window.location.href = "UIUXStady.html";
 };
